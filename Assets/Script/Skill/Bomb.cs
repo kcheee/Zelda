4aed890db6410378e24db0cf8f05ef65723e43d4
kcheee <cksgml1101@naver.com>
2023-07-04 10:40:01 +0900

    bomb
    bomb

--- Assets/Script/Skill/Bomb.cs
+++ Assets/Script/Skill/Bomb.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
@@ -16,5 +17,19 @@ public class Bomb : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
+
+        // 구 반경으로 위치를 가져옴.
+        Collider[] cols = Physics.OverlapSphere(collision.transform.position, 10);
+
+        for (int i = 0; i < cols.Length; i++)
+        {
+            Debug.Log(i+" : "+cols[i]);
+            // 폭탄 반경에 있는 오브젝트 rigidbody 가져옴
+            Rigidbody rigid = cols[i].GetComponent<Rigidbody>();
+            rigid.AddExplosionForce(200, this.transform.forward, 20, 5);
+        }
+       
+
+
     }
 }
