1799acb908c600fcb50c1a69f27d5f5b459781d2
kcheee <cksgml1101@naver.com>
2023-07-12 17:56:24 +0900

    pusg
    push

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-   
+    #region ½Ì±ÛÅæ ¹× Awake.
+    static public GameManager instance;
+    private void Awake()
+    {
+        instance = this;
+        StartCoroutine(StartUIdelay());
+    }
+    #endregion
+    
+    // StartUI
+    public GameObject StartUI;
+    IEnumerator StartUIdelay()
+    {
+        yield return new WaitForSeconds(1.5f);
+        StartUI.SetActive(true);
+        yield return new WaitForSeconds(4f);
+        StartUI.SetActive(false);
+    }
+
 }
+
+
+
+
