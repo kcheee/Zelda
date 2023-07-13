a971905721e8f1d8d67e96b85943dee3a5a5c8bb
kcheee <cksgml1101@naver.com>
2023-07-13 22:32:04 +0900

    push
    push

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        // 시작 UI
         StartCoroutine(Start_EndUIdelay());
     }
     #endregion
@@ -26,6 +27,7 @@ public class GameManager : MonoBehaviour
         Victory
     }
     public State state;
+    static Sequence sequenceFadeInOut;
 
     #region Start UI 및 End UI
     // 초기 시작 UI
@@ -45,16 +47,27 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 
+    // 클리어 타임 변수, koCnt 넣어야 함.
     #region 종료 UI
-
+    public CanvasGroup[] endUI;
+    public CanvasGroup BackGround;
+    
     IEnumerator EndUI()
     {
+        // 시작 UI 켜짐
         Start_EndUI.SetActive(true);
-        Vector3 UIPos = new Vector3(Start_EndUI.transform.position.x, Start_EndUI.transform.position.y + 85, Start_EndUI.transform.position.z);
-        // while 문 돌려야 함.
+        // victory 켜짐
+        yield return new WaitForSeconds(1f);
+        VictoryText.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(Fade(BackGround, 0f, 1f, 1f));
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(Fade(BackGround, 1f, 0.6f, 0.5f));
 
-        float dis = 999; Vector3.Distance(Start_EndUI.transform.position, UIPos);
-        // zoomPosition까지 Update 대체
+        Vector3 UIPos = new Vector3(Start_EndUI.transform.position.x, Start_EndUI.transform.position.y + 85, Start_EndUI.transform.position.z);
+
+        float dis = 999; // 임의의 수
+        // StartUI 위로 올라가는 코드.
         while (dis > 3)
         {
             Start_EndUI.transform.position = Vector3.Lerp(Start_EndUI.transform.position, UIPos, 0.2f);
@@ -62,20 +75,29 @@ public class GameManager : MonoBehaviour
             dis=Vector3.Distance(Start_EndUI.transform.position, UIPos);
             yield return new WaitForSeconds(0.02f);
         }
-        //while (true) 
-        //{
-        //    Vector3.Lerp(Start_EndUI.transform.position, UIPos, 0.5f);
-        //    if (Vector3.Distance(Start_EndUI.transform.position, UIPos) < 5)
-        //    {
-        //        break;
-        //    }
-        //}
-        //yield return new Null();
-        Debug.Log("tlfgod");
+
+        yield return new WaitForSeconds(0.5f);
+        for (int i = 0; i < 5; i++)
+        {
+            StartCoroutine(Fade(endUI[i], 0f, 1f, 0.05f));
+            yield return new WaitForSeconds(0.1f);
+        }
+       
     }
-
+    // fade 함수
+    private IEnumerator Fade(CanvasGroup group, float startAlpha, float targetAlpha, float duration)
+    {
+        float currentTime = 0f;
+        while (currentTime < duration)
+        {
+            float alpha = Mathf.Lerp(startAlpha, targetAlpha, currentTime / duration);
+            group.alpha = alpha;
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        group.alpha = targetAlpha;
+    }
     #endregion
-
 
     #region KillCount UI and KillcntUpdate() 함수
     // killUI
@@ -88,6 +110,8 @@ public class GameManager : MonoBehaviour
         KillCnt.text = kiilcnt.ToString();
     }
     #endregion
+
+
     private void Update()
     {
         // 킬수 테스트,  보코블린 Destory시에 KillcntUpdate()실행 해줘야 함.
