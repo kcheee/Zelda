a87e36c82858d0dd65eaf7d0cd2fa006e29760f3
kcheee <cksgml1101@naver.com>
2023-07-17 22:30:41 +0900

    slash delete
    slash delete

--- Assets/Script/Player/PlayerManager.cs
+++ Assets/Script/Player/PlayerManager.cs
@@ -7,9 +7,10 @@ public class PlayerManager : MonoBehaviour
     static public PlayerManager instance;
 
     public int hp;
-    int maxhp=6;
+    int maxhp = 6;
     public GameObject Healthbar;
-
+    GameObject[] health = new GameObject[6];
+    // HP Property
     public int HP
     {
         get { return hp; }
@@ -18,17 +19,35 @@ public class PlayerManager : MonoBehaviour
             hp = value;
         }
     }
+    int healthUI = 0;
+    
+    // 체력 데미지.
+    public void PlayerDamaged()
+    {
+        HP--;
+        health[healthUI].SetActive(false);
+        healthUI++;
+    }
     private void Awake()
     {
         HP = Healthbar.transform.childCount;
         instance = this;
+
+        for (int i = 0; i < maxhp; i++)
+        {
+            health[i] = Healthbar.transform.GetChild(i).gameObject;
+        }
     }
     public void potionHeal()
     {
-        Healthbar.transform.GetChild(HP-1).gameObject.SetActive(true);
+        Healthbar.transform.GetChild(HP - 1).gameObject.SetActive(true);
     }
     public void Update()
     {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            PlayerDamaged();
+        }
         //transform.position= Vector3.zero; 
     }
 }
